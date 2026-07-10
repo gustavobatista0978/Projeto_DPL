dad2040549e040b5d20bc1e15f1cd63c8e6cbe12
gustavobatista0978 <gustavobatista0978@gmail.com>
2026-07-10 10:48:42 -0300

    comhuds

---
+++ projeto/main.js
@@ -0,0 +1,9 @@
+let jogos = [
+    { id: 1, titulo: "The Last of Us Part II", plataforma: "ps5", disponivel: true },
+    { id: 2, titulo: "God of War Ragnarök", plataforma: "ps5", disponivel: true },
+    { id: 3, titulo: "The Legend of Zelda: Tears of the Kingdom", plataforma: "switch", disponivel: true },
+    { id: 4, titulo: "Elden Ring", plataforma: "xbox", disponivel: false },
+    { id: 5, titulo: "Forza Horizon 5", plataforma: "xbox", disponivel: true }
+];
+
+module.exports = jogos;
